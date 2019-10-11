466b341961a133a7e4a6bf6bacd91e73709112e2
Jesse Jafa <jesse.jafa@gmail.com>
2019-10-11 06:15:10 +0300

    removed antd for bulma, added babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const config = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
